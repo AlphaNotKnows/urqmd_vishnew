7d989f33efadab6b3ad84c3850366ba26422b222
AlphaNotKnows <qinjian.lou@Outlook.com>
2020-12-06 10:13:54 +0800

    add data of dNdeta0 of AuAu 200GeV

--- utillities/dNdy/plot_dNdeta.py
+++ utillities/dNdy/plot_dNdeta.py
@@ -1,18 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
-central=[0,3,6,10,15,20,25]
-for i in range(len(central)-1):
-  input_path="/home/qinjian/research/simulation/UrqmdVishnu/utillities/dNdy/DNDY/dNdeta{}~{}%.txt".format(central[i],central[i+1])
-  data=np.loadtxt(input_path)
-  data=data[4:20,:]
-  eta=data[:,0]
-  dNdeta=data[:,1]
-  plt.subplot(2,3,6-i)
-  plt.plot(eta,dNdeta,label="{}~{}%".format(central[i],central[i+1]))
-  plt.legend(fontsize=8)
-  plt.xticks(fontsize=8)
-  plt.yticks(np.arange(0,max(dNdeta)+50,100),fontsize=8)
-  # plt.xlabel(r"$\eta$")
-  # plt.ylabel(r"$dN/d\eta$")
-# plt.legend()
+
+PHOBOS=np.loadtxt("/home/qinjian/research/simulation/urqmd_vishnew/utillities/dNdy/central_AuAu200.txt")
+central=PHOBOS[:,0]
+ex_p=PHOBOS[:,1]
+error_p=PHOBOS[:,2]
+ex_pi=PHOBOS[:,3]
+error_pi=PHOBOS[:,4]
+ex_K=PHOBOS[:,5]
+error_K=PHOBOS[:,6]
+
+fig=plt.figure()
+ax=fig.add_axes([0.1,0.1,0.8,0.8])
+ax.errorbar(central,ex_p,yerr=error_p,label='PHOBOS p',fmt='o',ms=2)
+ax.errorbar(central,ex_pi,yerr=error_pi,label='PHOBOS pi',fmt='o',ms=2)
+ax.errorbar(central,ex_K,yerr=error_K,label='PHOBOS K',fmt='o',ms=2)
+ax.set_yscale('log')
+
+
+data=np.loadtxt("/home/qinjian/research/simulation/urqmd_vishnew/utillities/dNdy/dNdeta0_0.28_1.0.txt")
+p=data[:,2]
+pi=data[:,4]
+K=data[:,6]
+ax.plot(central,p,label='p')
+ax.plot(central,pi,label='pi')
+ax.plot(central,K,label='K')
+
+ax.legend()
+ax.set_title("Edec=1GeV")
 plt.show()
