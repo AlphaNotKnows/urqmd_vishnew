0197fe7eff4cf9d8c13ccc571486635b9629e584
AlphaNotKnows <qinjian.lou@Outlook.com>
2021-03-21 09:19:19 +0800

    change urqmd_frez.sh

--- urqmd-3.4/urqmd_frez.sh
+++ urqmd-3.4/urqmd_frez.sh
@@ -4,11 +4,11 @@ exename="urqmd"
 
 export ftn09=input_frez
 export ftn10=frez_start
-export ftn13=frez_result
+export ftn13=test.f13
 export ftn14=test.f14
 export ftn15=test.f15
 export ftn16=test.f16
-export ftn19=test.f19
+export ftn19=frez_result
 export ftn20=test.f20
 
 
